9ae34c4742ab154066893bcc95dd7e9a7d9bbaf6
Dil Pedro <mahcoisa@gmail.com>
2026-09-08 22:28:11 +0200

    TinyS3: lean boot (256MB upfront, ~470MB peak)

--- jvm/jvm-tinys3.js
+++ jvm/jvm-tinys3.js
@@ -104321,7 +104321,7 @@ assert(!Module["STACK_SIZE"], "STACK_SIZE can no longer be set at runtime.  Use 
 
 assert(typeof Int32Array != "undefined" && typeof Float64Array !== "undefined" && Int32Array.prototype.subarray != undefined && Int32Array.prototype.set != undefined, "JS engine does not provide full typed array support");
 
-var INITIAL_MEMORY = Module["INITIAL_MEMORY"] || 536870912;
+var INITIAL_MEMORY = Module["INITIAL_MEMORY"] || 268435456;
 
 legacyModuleProp("INITIAL_MEMORY", "INITIAL_MEMORY");
 
